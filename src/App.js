61ff75890632a1fa1920c56da27824263a79f83f
RostyslavDubinin <75220575+RostyslavDubinin@users.noreply.github.com>
2021-08-23 17:35:21 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PhoneBook from "./components/PhoneBook";
+
+const App = () => {
+  return (
+    <div>
+      <PhoneBook />
+    </div>
+  );
+};
+
+export default App;
